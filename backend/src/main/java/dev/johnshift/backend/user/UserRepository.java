e3f391c40d4cfdb8729912029646416585b0672a
John Ballesteros <johnshift.dev@gmail.com>
2021-10-10 21:03:49 +0800

    added backend files, closes #2

---
+++ backend/src/main/java/dev/johnshift/backend/user/UserRepository.java
@@ -0,0 +1,7 @@
+package dev.johnshift.backend.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+  
+}
---
+++ backend/src/main/resources/db/users_sample.sql
@@ -0,0 +1,4 @@
+INSERT INTO users (username, password) VALUES 
+  ('user1', 'YAu0A22EzG7LrAG4egWvmTBHyGGMPdyejdYq8qBHYVK5d7nPCOVvnPdQNVjDpRig'), 
+  ('user2', 'ZxDKsUpC0041tLYCBaeFDroC13SUQZ6hB8bxahBuEWTK3jpRbpwUSzcyxTjTZnxq'),
+  ('user3', 's5OeY8t7VX07PuJ2ov75qiO59Es5QhEgat9YJZli8322UIuPpoNIwdMTknE06aLZ');
