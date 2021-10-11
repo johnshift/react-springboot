8a83e4cd91daec42dfc54a04e53a3cef7db78c0c
John Ballesteros <johnshift.dev@gmail.com>
2021-10-11 10:35:14 +0800

    added vercel preview deployment

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -32,7 +32,7 @@ const App = (): JSX.Element => {
           <Image src={logo} alt="" h="40vmin" />
         </motion.div>
         <Text fontSize={textFontSizes}>
-          Hello Vite + React + Typescript + Chakra UI! + change #3
+          Hello Vite + React + Typescript + Chakra UI! + change #4
         </Text>
         <Button
           colorScheme="blue"
