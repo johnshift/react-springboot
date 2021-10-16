8fb82a491b446e03a7f9b002c8fbb77f0734fe9a
John Ballesteros <johnshift.dev@gmail.com>
2021-10-17 00:21:39 +0800

    added hardcoded auth redirect

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -8,36 +8,42 @@ import Home from './pages/home';
 import PageLoader from './components/PageLoader';
 import NotFound from './pages/404';
 
-const Main = () => (
-  <Router>
-    <Route path="/" component={Home} />
-    <AsyncRoute
-      path="/landing"
-      getComponent={() => import('./pages/landing').then((module) => module.default)}
-      loading={() => <PageLoader />}
-    />
-    <AsyncRoute
-      path="/profile"
-      getComponent={() => import('./pages/profile').then((module) => module.default)}
-      loading={() => <PageLoader />}
-    />
-    <AsyncRoute
-      path="/alias"
-      getComponent={() => import('./pages/alias').then((module) => module.default)}
-      loading={() => <PageLoader />}
-    />
-    <AsyncRoute
-      path="/group"
-      getComponent={() => import('./pages/group').then((module) => module.default)}
-      loading={() => <PageLoader />}
-    />
-    <AsyncRoute
-      path="/counter"
-      getComponent={() => import('./pages/counter').then((module) => module.default)}
-      loading={() => <PageLoader />}
-    />
-    <Route component={NotFound} default />
-  </Router>
-);
+const Main = () => {
+  // this should be done on a state mechanism
+  // that either uses jwt or backend etc etc
+  // and stores to local state
+  const loggedIn = false;
+  const isLoggedIn = (): boolean => {
+    return loggedIn;
+  };
+
+  return (
+    <Router>
+      <Home path="/" loggedIn={isLoggedIn()} />
+
+      <AsyncRoute
+        path="/profile"
+        getComponent={() => import('./pages/profile').then((module) => module.default)}
+        loading={() => <PageLoader />}
+      />
+      <AsyncRoute
+        path="/alias"
+        getComponent={() => import('./pages/alias').then((module) => module.default)}
+        loading={() => <PageLoader />}
+      />
+      <AsyncRoute
+        path="/group"
+        getComponent={() => import('./pages/group').then((module) => module.default)}
+        loading={() => <PageLoader />}
+      />
+      <AsyncRoute
+        path="/counter"
+        getComponent={() => import('./pages/counter').then((module) => module.default)}
+        loading={() => <PageLoader />}
+      />
+      <Route component={NotFound} default />
+    </Router>
+  );
+};
 
 render(<Main />, document.getElementById('app')!);
--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,7 +1,20 @@
-const Home = () => (
-  <div class="flex container mx-auto h-screen justify-center items-center">
-    <h1 class="text-6xl">Home Page</h1>
-  </div>
-);
+import Landing from '../landing';
+
+type Props = {
+  loggedIn: boolean;
+  path: string;
+};
+
+const Home = ({ loggedIn }: Props) => {
+  if (!loggedIn) {
+    return <Landing />;
+  }
+
+  return (
+    <div class="flex container mx-auto h-screen justify-center items-center">
+      <h1 class="text-6xl">Home Page</h1>
+    </div>
+  );
+};
 
 export default Home;
