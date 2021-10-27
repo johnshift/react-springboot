dfea4080a52322049ba63e124f76ac5f8ea79766
John Ballesteros <johnshift.dev@gmail.com>
2021-10-27 16:48:25 +0800

    adjust responsive breakpoints

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,23 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
+
+import { extendTheme } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
+
 import Nav from "../components/Nav";
+
+const breakpoints = createBreakpoints({
+  sm: "48em", // tab
+  md: "62em", // small screen
+  lg: "85em", // laptop common
+  xl: "96em", // hd
+});
+
+const theme = extendTheme({ breakpoints });
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Nav />
       <Component {...pageProps} />{" "}
     </ChakraProvider>
