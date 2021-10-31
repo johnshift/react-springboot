5d5679b746ff0ce022bc0a6467f513d71ecc765c
John Ballesteros <johnshift.dev@gmail.com>
2021-10-31 13:19:52 +0800

    add emoji selector function to textarea

--- frontend/src/components/CreatePost.tsx
+++ frontend/src/components/CreatePost.tsx
@@ -17,7 +17,7 @@ import {
   ModalHeader,
   ModalBody,
 } from "@chakra-ui/react";
-import { useState } from "react";
+import { MutableRefObject, useEffect, useRef, useState } from "react";
 import { MdPerson } from "react-icons/md";
 import { GiDoubleFaceMask } from "react-icons/gi";
 import { IoMdHeart, IoIosAt, IoIosCalendar } from "react-icons/io";
@@ -41,91 +41,6 @@ const CreatePostImage = () => (
     </Center>
   </GridItem>
 );
-
-const CreatePostInput = () => (
-  <GridItem
-    px={[3, 2]}
-    pr={1}
-    pb={2}
-    rowSpan={4}
-    colSpan={[10, 9, 10]}
-    // bg="blue.200"
-    // border="1px solid teal"
-  >
-    <Textarea
-      placeholder="Share your secrets anonymously"
-      h={"100%"}
-      resize="none"
-      border="1px solid red"
-      colorScheme="blue"
-      // variant="filled"
-    />
-  </GridItem>
-);
-
-const CreatePostOptions = () => {
-  const {
-    isOpen: isOpenEmoji,
-    onOpen: onOpenEmoji,
-    onClose: onCloseEmoji,
-  } = useDisclosure();
-
-  return (
-    <GridItem
-      rowSpan={2}
-      colSpan={[5, 5, 6]}
-      // bg="cyan.200"
-      // border="1px solid teal"
-      ml={[-1, 1]}
-    >
-      {/* <Center h="100%"> */}
-      <Flex align="center" justify="start" h="100%">
-        <IconButton
-          color="blackt"
-          variant="outline"
-          border="none"
-          aria-label="select emoji"
-          icon={<Icon as={IoMdHeart} w={6} h={6} />}
-          isRound
-          onClick={onOpenEmoji}
-        />
-        <IconButton
-          color="blackt"
-          variant="outline"
-          border="none"
-          aria-label="mention someone"
-          icon={<Icon as={IoIosAt} w={6} h={6} />}
-          isRound
-        />
-        <IconButton
-          color="blackt"
-          variant="outline"
-          border="none"
-          aria-label="dark mode"
-          icon={<Icon as={IoIosCalendar} w={6} h={6} />}
-          isRound
-        />
-
-        <Modal
-          isOpen={isOpenEmoji}
-          onClose={onCloseEmoji}
-          isCentered
-          scrollBehavior="inside"
-          blockScrollOnMount={false}
-        >
-          <ModalOverlay />
-          <ModalContent w="300px" maxH="300px">
-            <ModalHeader textAlign="center">Select Emoji</ModalHeader>
-            <ModalBody>
-              <EmojiSelector />
-            </ModalBody>
-          </ModalContent>
-        </Modal>
-      </Flex>
-      {/* </Center> */}
-    </GridItem>
-  );
-};
 
 const CreatePostButton = () => {
   const toast = useToast();
@@ -166,25 +81,122 @@ const CreatePostButton = () => {
   );
 };
 
-const CreatePost = () => (
-  <Grid
-    bg="whiter"
-    // border="1px solid teal"
-    borderRadius="lg"
-    shadow="md"
-    mb={5}
-    h={200}
-    templateRows="repeat(6, 1fr)"
-    templateColumns="repeat(12, 1fr)"
-    px={2}
-    pb={3}
-    pt={4}
-  >
-    <CreatePostImage />
-    <CreatePostInput />
-    <CreatePostOptions />
-    <CreatePostButton />
-  </Grid>
-);
+const CreatePost = () => {
+  const [postBody, setPostBody] = useState("");
+
+  const finalRef = useRef() as MutableRefObject<HTMLTextAreaElement>;
+  useEffect(() => {
+    // fix: textarea focus cursor to the end after emoji eselection
+    finalRef.current.selectionStart = finalRef.current.value.length;
+    finalRef.current.selectionEnd = finalRef.current.value.length;
+  }, [postBody]);
+
+  const {
+    isOpen: isOpenEmoji,
+    onOpen: onOpenEmoji,
+    onClose: onCloseEmoji,
+  } = useDisclosure();
+
+  return (
+    <Grid
+      bg="whiter"
+      // border="1px solid teal"
+      borderRadius="lg"
+      shadow="md"
+      mb={5}
+      h={200}
+      templateRows="repeat(6, 1fr)"
+      templateColumns="repeat(12, 1fr)"
+      px={2}
+      pb={3}
+      pt={4}
+    >
+      <CreatePostImage />
+
+      {/* BODY */}
+      <GridItem
+        px={[3, 2]}
+        pr={1}
+        pb={2}
+        rowSpan={4}
+        colSpan={[10, 9, 10]}
+        // bg="blue.200"
+        // border="1px solid teal"
+      >
+        <Textarea
+          ref={finalRef}
+          placeholder="Share your secrets anonymously"
+          h={"100%"}
+          resize="none"
+          border="1px solid red"
+          colorScheme="blue"
+          value={postBody}
+          onChange={(e) => {
+            setPostBody(e.target.value);
+          }}
+          autoFocus
+          // variant="filled"
+          onFocus={(e) => {
+            e.preventDefault();
+          }}
+        />
+      </GridItem>
+
+      {/* OPTIONS */}
+      <GridItem rowSpan={2} colSpan={[5, 5, 6]} ml={[-1, 1]}>
+        <Flex align="center" justify="start" h="100%">
+          <IconButton
+            color="blackt"
+            variant="outline"
+            border="none"
+            aria-label="select emoji"
+            icon={<Icon as={IoMdHeart} w={6} h={6} />}
+            isRound
+            onClick={onOpenEmoji}
+          />
+          <IconButton
+            color="blackt"
+            variant="outline"
+            border="none"
+            aria-label="mention someone"
+            icon={<Icon as={IoIosAt} w={6} h={6} />}
+            isRound
+          />
+          <IconButton
+            color="blackt"
+            variant="outline"
+            border="none"
+            aria-label="dark mode"
+            icon={<Icon as={IoIosCalendar} w={6} h={6} />}
+            isRound
+          />
+
+          <Modal
+            isOpen={isOpenEmoji}
+            onClose={onCloseEmoji}
+            isCentered
+            scrollBehavior="inside"
+            blockScrollOnMount={false}
+            finalFocusRef={finalRef}
+          >
+            <ModalOverlay />
+            <ModalContent w="300px" maxH="300px">
+              <ModalHeader textAlign="center">Select Emoji</ModalHeader>
+              <ModalBody>
+                <EmojiSelector
+                  postBody={postBody}
+                  setPostBody={setPostBody}
+                  closeEmojiSelector={onCloseEmoji}
+                />
+              </ModalBody>
+            </ModalContent>
+          </Modal>
+        </Flex>
+      </GridItem>
+
+      <CreatePostButton />
+    </Grid>
+  );
+};
 
 export default CreatePost;
