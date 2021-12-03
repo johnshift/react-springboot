96e609e3834bd1f4f6dc90b5b705680030bf1378
John Ballesteros <johnshift.dev@gmail.com>
2021-12-03 22:16:05 +0800

    cleanup after enabling cicd

--- backend/src/main/java/dev/johnshift/backend/credential/CredentialService.java
+++ backend/src/main/java/dev/johnshift/backend/credential/CredentialService.java
@@ -4,26 +4,6 @@ import java.util.Optional;
 
 public interface CredentialService {
 
-	// /** This is mainly used for implementing {@link UserDetails}.
-	// * <p>
-	// * Note that {@link UserDetails} only needs a principal (username/email) and credential
-	// (password).
-	// * Retrieves using username, returns null instead of throwing exception.
-	// *
-	// * @param username
-	// * @return */
-	// String getPasswordByUsernameOrNull(String username);
-
-	// /** This is mainly used for implementing {@link UserDetails}.
-	// * <p>
-	// * Note that {@link UserDetails} only needs a principal (username/email) and credential
-	// (password).
-	// * Retrieves using email, returns null instead of throwing exception.
-	// *
-	// * @param email
-	// * @return */
-	// String getPasswordByEmailOrNull(String email);
-
 	/** This is used when retrieving password using either username or email.
 	 * 
 	 * @param principal
