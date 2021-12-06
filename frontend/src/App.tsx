4cb1ae5329cff1d0b8c4b3f83da8e840a4081b5a
John Ballesteros <johnshift.dev@gmail.com>
2021-12-06 20:25:18 +0800

    working wouter + windicss

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,17 @@
-import { useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'virtual:windi.css';
+import { Route, Switch } from 'wouter';
+import Login from './pages/Login';
+
+const Home = () => <h1>Home Page</h1>;
+const NotFound = () => <h1>Not Found</h1>;
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer">
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer">
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route component={NotFound} />
+    </Switch>
   );
 }
 
