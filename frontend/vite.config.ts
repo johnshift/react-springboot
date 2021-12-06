664172142329fa17aee1ab705d842bfaa8416959
John Ballesteros <johnshift.dev@gmail.com>
2021-12-06 19:43:05 +0800

    add plain react-ts vite

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
-import WindiCSS from 'vite-plugin-windicss'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  esbuild: {
-    jsxFactory: 'h',
-    jsxFragment: 'Fragment',
-  },
-  plugins: [preact(), WindiCSS()],
+  plugins: [react()]
 })
