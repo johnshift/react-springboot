b4f4af04256b5347908860e31bcbf57ff44b5f94
John Ballesteros <johnshift.dev@gmail.com>
2021-12-06 23:24:09 +0800

    working frontend jest mock backend-api

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,7 +1,5 @@
-let BACKEND_URL = 'https://veils.heroku.app/api/v1';
-if (import.meta.env.DEV) {
-  BACKEND_URL = 'http://localhost:8080/api/v1';
-}
+// const BACKEND_URL = 'https://veils.heroku.app/api/v1';
+const BACKEND_URL = 'http://localhost:8080/api/v1';
 
 export const LOGIN_URI = BACKEND_URL + '/login';
 
