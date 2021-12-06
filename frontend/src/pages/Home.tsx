d5486eca8da953ac877e3e61811b7bba298f7634
John Ballesteros <johnshift.dev@gmail.com>
2021-12-06 21:42:20 +0800

    implement logout

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,15 +6,23 @@ const Home = () => {
   const [, setLocation] = useLocation();
 
   useEffect(() => {
-    const token = localStorage.getItem(JWT_HEADER_KEY);
-    if (!token) {
+    if (!localStorage.getItem(JWT_HEADER_KEY)) {
       setLocation('/login');
     }
   }, []);
 
+  const logout = () => {
+    localStorage.removeItem(JWT_HEADER_KEY);
+    alert('You have successfully logged out!');
+    setLocation('/login');
+  };
+
   return (
     <div className="flex conainer mx-auto h-screen justify-center items-center">
       <p>Home Page</p>
+      <br />
+      <br />
+      <button onClick={logout}>LOGOUT</button>
     </div>
   );
 };
