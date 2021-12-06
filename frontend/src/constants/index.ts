bbb608067a049fb128065d929f123d4496e8f61c
John Ballesteros <johnshift.dev@gmail.com>
2021-12-06 19:43:05 +0800

    add plain react-ts vite

--- frontend/src/constants/index.ts
+++
@@ -1,9 +0,0 @@
-// URI
-let BACKEND_URL = 'https://veils.heroku.app/api/v1';
-if (import.meta.env.DEV) {
-  BACKEND_URL = 'http://localhost:8080/api/v1';
-}
-
-export const LOGIN_URI = BACKEND_URL + '/login';
-export const AUTHORIZATION_HEADER_KEY = 'authorization';
-export const JWT_LOCALSTORAGE_KEY = 'jwt-key';
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,22 +1,11 @@
-import 'virtual:windi.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
 
-import { Router, Route } from 'preact-router';
-import AsyncRoute from 'preact-async-route';
-import { render } from 'preact';
-
-import Home from './pages/Home';
-import Login from './pages/Login';
-
-const Main = () => (
-	<Router>
-		<Route path="/" component={Home} />
-		<Route path="/login" component={Login} />
-		<AsyncRoute
-			path="/counter"
-			getComponent={() => import('./pages/Counter').then((module) => module.default)}
-			loading={() => <div>loading...</div>}
-		/>
-	</Router>
-);
-
-render(<Main />, document.getElementById('app')!);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+)
