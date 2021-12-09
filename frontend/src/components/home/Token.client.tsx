20cafb5dbd444cf50087a133de5618623c3a79b4
John Ballesteros <johnshift.dev@gmail.com>
2021-12-10 00:27:21 +0800

    fix bugged toasts

--- frontend/src/components/home/Token.client.tsx
+++ frontend/src/components/home/Token.client.tsx
@@ -1,30 +1,22 @@
-import Router from "next/router";
+import { useRouter } from "next/router";
 import { useEffect } from "react";
 import toast from "react-hot-toast";
-import { MSG_ALREADY_LOGGEDIN, TOAST_OPTIONS } from "../../lib/constants";
+import { MSG_PLEASE_LOGIN, TOAST_OPTIONS } from "../../lib/constants";
 import { useAuth } from "../../lib/contexts/auth";
 import Button from "../common/Button";
 import Spinner from "../common/Spinner";
 import PageCenter from "../layout/PageCenter";
 
 const Token = () => {
-  const {
-    authLoading,
-    isAuthenticated,
-    setIsRedirected,
-    isRedirected,
-    authLogout,
-  } = useAuth();
+  const router = useRouter();
+
+  const { authLoading, isAuthenticated, authLogout } = useAuth();
 
   useEffect(() => {
     if (!authLoading && !isAuthenticated) {
-      setIsRedirected(true);
-      Router.replace("/login");
-    }
-
-    if (isRedirected && isAuthenticated) {
-      setIsRedirected(false);
-      toast(MSG_ALREADY_LOGGEDIN, TOAST_OPTIONS);
+      router.replace("/login").then(() => {
+        toast(MSG_PLEASE_LOGIN, TOAST_OPTIONS);
+      });
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isAuthenticated, authLoading]);
--- frontend/src/lib/contexts/auth.tsx
+++ frontend/src/lib/contexts/auth.tsx
@@ -1,51 +1,45 @@
 import { useRouter } from "next/router";
 import {
   createContext,
-  Dispatch,
   ReactNode,
-  SetStateAction,
   useContext,
   useEffect,
   useState,
 } from "react";
-import { AUTHORIZATION_KEY } from "../constants";
+import toast from "react-hot-toast";
+import {
+  AUTHORIZATION_KEY,
+  MSG_SUCCESSFUL_LOGOUT,
+  TOAST_OPTIONS,
+} from "../constants";
 
 type Props = {
   children: ReactNode;
 };
 
 type AuthContextT = {
-  isRedirected: boolean;
   isAuthenticated: boolean;
   authLoading: boolean;
-  justLoggedOut: boolean;
-  setIsRedirected: (redirected: boolean) => void;
   authLogin: (token?: string) => void;
   authLogout: () => void;
-  setJustLoggedOut: (justLoggedOut: boolean) => void;
 };
 
 const AuthContext = createContext<AuthContextT>({
-  isRedirected: false,
   isAuthenticated: false,
   authLoading: true,
-  justLoggedOut: false,
-  setIsRedirected: (redirected: boolean) => {},
   authLogin: () => {},
   authLogout: () => {},
-  setJustLoggedOut: () => {},
 });
 
 export const AuthProvider = ({ children }: Props) => {
   const router = useRouter();
-  const [isRedirected, setIsRedirected] = useState(false);
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [authLoading, setAuthLoading] = useState(true);
-  const [justLoggedOut, setJustLoggedOut] = useState(false);
 
   useEffect(() => {
+    console.log("AUthProvider useEffect called");
     const token = localStorage.getItem(AUTHORIZATION_KEY);
-    console.log("AuthProvider token: ", token);
+    console.log("AuthProvider token from localStorage: ", token);
 
     // to-do: replace this with jwt-validation
     if (token !== null && token !== undefined) {
@@ -55,37 +49,36 @@ export const AuthProvider = ({ children }: Props) => {
   }, []);
 
   const authLogin = (token?: string) => {
+    // no network calls. no need to display loaders
+
     setIsAuthenticated(true);
 
     if (token) {
       localStorage.setItem(AUTHORIZATION_KEY, token);
     }
+
+    setAuthLoading(false);
+
     // to-do: get info using claims token
   };
 
   const authLogout = () => {
-    // simulate logout delay
-    setAuthLoading(true);
-    setTimeout(() => {
-      setIsAuthenticated(false);
-      localStorage.removeItem(AUTHORIZATION_KEY);
-      setJustLoggedOut(false);
-      setAuthLoading(false);
-      router.push("/login");
-    }, 3000);
+    // no network calls. no need to display loaders
+
+    setIsAuthenticated(false);
+    localStorage.removeItem(AUTHORIZATION_KEY);
+    router.push("/login").then(() => {
+      toast.success(MSG_SUCCESSFUL_LOGOUT, TOAST_OPTIONS);
+    });
   };
 
   return (
     <AuthContext.Provider
       value={{
-        isRedirected,
         isAuthenticated,
         authLoading,
-        justLoggedOut,
         authLogin,
         authLogout,
-        setIsRedirected,
-        setJustLoggedOut,
         // userDetails
       }}
     >
