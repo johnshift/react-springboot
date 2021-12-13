48f0a3f179ab474a71c830a7194903dda7476d13
John Ballesteros <johnshift.dev@gmail.com>
2021-12-13 17:45:32 +0800

    try switch tailwindcss -> windicss

---
+++ frontend/src/lib/constants.ts
@@ -0,0 +1,2 @@
+export const DEFAULT_PAGE_TITLE = "Veils";
+export const DEFAULT_PAGE_DESCRIPTION = "Share your secrets anonymously";
--- frontend/src/lib/simulateDelay.ts
+++
@@ -1,5 +0,0 @@
-import { randNum } from "./randNum";
-
-export const simulateDelay = () => {
-  return new Promise((r) => setTimeout(r, randNum(1000, 2000)));
-};
