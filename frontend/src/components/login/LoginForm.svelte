d55d38ea5695dcd6378c2990f7dd8bea7c664d39
John Ballesteros <johnshift.dev@gmail.com>
2021-12-15 05:45:22 +0800

    implement fetch for login api

--- frontend/src/components/login/LoginForm.svelte
+++ frontend/src/components/login/LoginForm.svelte
@@ -1,40 +1,60 @@
 <script lang="ts">
   import {
+    JSON_HEADERS,
+    LOGIN_API_URL,
+    MSG_SOMETHING_WENT_WRONG,
+    MSG_SUCCESSFUL_LOGIN,
+  } from "../../lib/constants";
+
+  import {
     NotificationType,
     notify,
     dismissNotification,
   } from "../../store/NotificationStore";
   import sleep from "../../lib/utils/sleep";
+  import { MessageResponse } from "../../lib/types";
 
   let principal = "";
   let password = "";
 
-  let message = "Something went wrong :(";
+  // set default notification
+  let message = MSG_SOMETHING_WENT_WRONG;
   let notificationType: NotificationType = "error";
+
   let hasError = false;
   let isLoading = false;
 
-  const submit = async (e: Event) => {
+  const submit = async () => {
     clear();
     isLoading = true;
     notify("Loading please wait", "loading");
 
     await sleep(2000);
 
-    const formData = new FormData(e.target as HTMLFormElement);
+    try {
+      const response = await fetch(LOGIN_API_URL, {
+        method: "POST",
+        body: JSON.stringify({ principal, password }),
+        headers: JSON_HEADERS,
+      });
 
-    const data = {};
-    for (let field of formData) {
-      const [key, value] = field;
-      data[key] = value;
+      const jsonResponse = (await response.json()) as MessageResponse;
+      if (!response.ok) {
+        throw jsonResponse.message;
+      }
+
+      message = MSG_SUCCESSFUL_LOGIN;
+      notificationType = "success";
+    } catch (errmsg) {
+      if (typeof errmsg === "string") {
+        message = errmsg;
+        hasError = true;
+        return;
+      }
+    } finally {
+      isLoading = false;
+      notify(message, notificationType);
     }
-
-    // todo: fetch request (simulate for now)
-    message = "Incorrect username/email or password";
-    hasError = true;
-
-    isLoading = false;
-    notify(message, notificationType);
   };
 
   const clear = () => {
