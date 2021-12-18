37c51686d7e1441ccd232e41744e843f0a37ef76
John Ballesteros <johnshift.dev@gmail.com>
2021-12-18 10:46:43 +0800

    add localhost (temporary) in allowed origins

--- backend/src/main/java/dev/johnshift/backend/configs/CorsConfig.java
+++ backend/src/main/java/dev/johnshift/backend/configs/CorsConfig.java
@@ -2,14 +2,12 @@ package dev.johnshift.backend.configs;
 
 import java.util.Arrays;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 public class CorsConfig {
@@ -21,10 +19,11 @@ public class CorsConfig {
 
 		List<String> allowedOrigins = Arrays.asList(
 			"https://veils-dev.vercel.app",
-			"https://veils.vercel.app"
-		// "http://localhost:3000", // WARNING: remove localhost in production!
-		// "http://localhost:5000" // WARNING: remove localhost in production!
-		);
+			"https://veils.vercel.app",
+
+			// todo: disable localhost origins on production
+			"http://localhost:3000",
+			"http://localhost:5000");
 
 		List<String> allowedHeaders = Arrays.asList(
 			"Content-Type",
