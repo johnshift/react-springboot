d265b040ceab1b22a4034116aa8926f77ce0454b
John Ballesteros <johnshift.dev@gmail.com>
2021-12-19 20:43:46 +0800

    working login-validation + tests

--- e2e-tests/cypress/support/commands.js
+++ e2e-tests/cypress/support/commands.js
@@ -23,48 +23,46 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-import '@testing-library/cypress/add-commands';
+import "@testing-library/cypress/add-commands";
 
-Cypress.Commands.add('login', (principal, password) => {
+Cypress.Commands.add("login", (principal, password) => {
+  if (principal === "" || password === "") {
+    cy.get("@login-btn").focus().click();
+    return;
+  }
 
-	if (principal === '' || password === '') {
-		cy.get('@login-btn').click();
-		return;
-	}
+  cy.get("@principal").type(principal);
 
-	cy.get('@principal')
-		.type(principal)
+  cy.get("@password").type(password);
 
-	cy.get('@password')
-		.type(password)
+  cy.get("@login-btn").focus().click();
+});
 
-	cy.get('@login-btn')
-		.click()
+Cypress.Commands.add("loginLoading", (showsSkeleton) => {
+  console.log("showsSkeleton: ", showsSkeleton);
 
-})
+  if (showsSkeleton) {
+    // form should not exist
+    cy.get("form").should("not.exist");
 
-Cypress.Commands.add('loginLoading', () => {
-	// form should not exist
-		cy.get('form')
-			.should('not.exist')
-		
-		// login skeleton exists
-		cy.get('#login-skeleton')
-			.should('exist')
+    // login skeleton exists
+    cy.get("#login-skeleton").should("exist");
 
-		// should display loading status
-		cy.contains(/loading*/i)
-			.should('be.visible')
-})
+    // should display loading status
+    cy.contains(/loading*/i).should("be.visible");
+    return;
+  }
 
-Cypress.Commands.add('loginError', (errmsg) => {
-		// wait until it should display error
-		cy.contains(errmsg)
-			.should('be.visible')
+  cy.get("form").should("exist");
+  cy.contains("#login-skeleton").should("not.exist");
+  cy.contains(/loading*/i).should("not.exist");
+});
 
-		// inputs should have red borders
-		cy.get('@principal')
-			.should('have.class', 'border-red-300')	
-		cy.get('@password')
-			.should('have.class', 'border-red-300')
-})
+Cypress.Commands.add("loginError", (errmsg) => {
+  // wait until it should display error
+  cy.contains(errmsg).should("be.visible");
+
+  // inputs should have red borders
+  cy.get("@principal").should("have.class", "border-red-300");
+  cy.get("@password").should("have.class", "border-red-300");
+});
