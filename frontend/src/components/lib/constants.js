b17babad27daf7826054439215dad851688a406b
John Ballesteros <johnshift.dev@gmail.com>
2021-12-19 13:06:30 +0800

    add show loading msg

--- frontend/src/components/lib/constants.js
+++ frontend/src/components/lib/constants.js
@@ -2,5 +2,6 @@ export const BACKEND_API_URL = import.meta.env.PUBLIC_BACKEND_API_URL;
 
 export const MSG_SOMETHING_WENT_WRONG = 'Something went wrong :(';
 export const MSG_LOGIN_SUCCESSFUL = 'Login successful'
+export const MSG_LOADING = 'Loading please wait'
 
 export const KEY_AUTHORIZATION = 'authorization';
--- frontend/src/components/login/LoginForm.jsx
+++ frontend/src/components/login/LoginForm.jsx
@@ -1,28 +1,32 @@
-import { useState } from 'preact/hooks'
-import { notify } from '../../store/NotificationStore'
-import { BACKEND_API_URL, KEY_AUTHORIZATION, MSG_SOMETHING_WENT_WRONG } from '../lib/constants'
+import { useState } from "preact/hooks";
+import { notify } from "../../store/NotificationStore";
+import {
+  BACKEND_API_URL,
+  KEY_AUTHORIZATION,
+  MSG_LOADING,
+  MSG_SOMETHING_WENT_WRONG,
+} from "../lib/constants";
 
 const Skeleton = () => {
-	return (
-		<div class='animate-pulse pb-1'>
-			<div class='bg-gray-300 w-full mb-10 h-12 rounded-lg' />
-			<div class='bg-gray-300 w-full mb-10 h-12 rounded-lg' />
-			<div className="flex justify-between items-center">
-				<div class='bg-gray-300 w-6/12 h-6' />
-				<div class='bg-gray-300 w-24 px-4 h-10 rounded-lg' />
-			</div>
-		</div>
-	)
-}
+  return (
+    <div class="animate-pulse pb-1">
+      <div class="bg-gray-300 w-full mb-10 h-12 rounded-lg" />
+      <div class="bg-gray-300 w-full mb-10 h-12 rounded-lg" />
+      <div className="flex justify-between items-center">
+        <div class="bg-gray-300 w-6/12 h-6" />
+        <div class="bg-gray-300 w-24 px-4 h-10 rounded-lg" />
+      </div>
+    </div>
+  );
+};
 
 const LoginForm = () => {
-
-	const [payload, setPayload] = useState({
-		principal: '',
-		password: '',
-	});
-	const [loading, setLoading] = useState(false);
-	const [hasError, setHasError] = useState(false);
+  const [payload, setPayload] = useState({
+    principal: "",
+    password: "",
+  });
+  const [loading, setLoading] = useState(false);
+  const [hasError, setHasError] = useState(false);
 
   const handleChange = (e) => {
     setPayload({
@@ -31,81 +35,90 @@ const LoginForm = () => {
     });
   };
 
-	const handleSubmit = async (e) => {
-		e.preventDefault();
-		setLoading(true)
-		await new Promise(r => setTimeout(r,1000))
+  const setLoadingIndicator = (isLoading) => {
+    setLoading(isLoading);
+    if (isLoading) {
+      notify(MSG_LOADING, "loading");
+    }
+  };
 
-		let notifType = 'error';
-		let notifMessage = MSG_SOMETHING_WENT_WRONG
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoadingIndicator(true);
+    await new Promise((r) => setTimeout(r, 1000));
 
-		try {
+    let notifType = "error";
+    let notifMessage = MSG_SOMETHING_WENT_WRONG;
 
-			const response = await fetch(BACKEND_API_URL + '/login', {
-				method: "POST",
-				body: JSON.stringify(payload),
-			});
+    try {
+      const response = await fetch(BACKEND_API_URL + "/login", {
+        method: "POST",
+        body: JSON.stringify(payload),
+      });
 
-			// login with errors
-			if (!response.ok) {
-				const { message } = await response.json();
-				setHasError(true);
-				notifMessage = message;
-				return;
-			}
+      // login with errors
+      if (!response.ok) {
+        const { message } = await response.json();
+        setHasError(true);
+        notifMessage = message;
+        return;
+      }
 
-			// successful login
-			notifMessage = 'Login successful'
-			localStorage.setItem(KEY_AUTHORIZATION, response.headers.get(KEY_AUTHORIZATION))
-		} finally {
-			notify(notifMessage, notifType);
-			setLoading(false)
-		}
-	}
-	
+      // successful login
+      notifMessage = "Login successful";
+      localStorage.setItem(
+        KEY_AUTHORIZATION,
+        response.headers.get(KEY_AUTHORIZATION)
+      );
+    } finally {
+      notify(notifMessage, notifType);
+      setLoadingIndicator(false);
+    }
+  };
 
-	const border = hasError ? " border-red-300" : "";
+  const border = hasError ? " border-red-300" : "";
   const inputClassName = "w-full mb-10" + border;
 
-	return (
-			<div class="p-10 rounded-lg shadow-md w-11/12 lg:w-9/12 hd:w-8/12 border border-gray-300">
-		{
-			loading ? <Skeleton /> : 
-			<form onSubmit={handleSubmit}>
-				
-				<input
-					name="principal"
-					placeholder="Username or Email"
-					className={inputClassName}
-					onChange={handleChange}
-					value={payload.principal}
-				/>
+  return (
+    <div class="p-10 rounded-lg shadow-md w-11/12 lg:w-9/12 hd:w-8/12 border border-gray-300">
+      {loading ? (
+        <Skeleton />
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <input
+            name="principal"
+            placeholder="Username or Email"
+            className={inputClassName}
+            onChange={handleChange}
+            value={payload.principal}
+          />
 
-				<input
-					name="password" type="password"
-					placeholder="Password"
-					className={inputClassName}
-					onChange={handleChange}
-					value={payload.password}
-				/>
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            className={inputClassName}
+            onChange={handleChange}
+            value={payload.password}
+          />
 
-				<div className="flex justify-between items-center">
-					<div>
-						<a href="/signup">Create an account</a>
-					</div>
-					<div>
-						<button
-							type="submit"
-							className="w-24 text-white font-semibold bg-red-700 hover:bg-red-600 hover:text-white "
-						>
-							Login
-						</button>
-					</div>
-				</div>
-	    </form>
-		}
-		</div>
-	)
-}
+          <div className="flex justify-between items-center">
+            <div>
+              <a href="/signup">Create an account</a>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="w-24 text-white font-semibold bg-red-700 hover:bg-red-600 hover:text-white "
+              >
+                Login
+              </button>
+            </div>
+          </div>
+        </form>
+      )}
+    </div>
+  );
+};
 
-export default LoginForm
+export default LoginForm;
