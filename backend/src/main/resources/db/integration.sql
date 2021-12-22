fd4d03a4499cb1660569db0a9044b2422eeefa63
John Ballesteros <johnshift.dev@gmail.com>
2021-12-23 07:30:42 +0800

    manually created unique constraints

--- backend/src/main/resources/db/integration.sql
+++ backend/src/main/resources/db/integration.sql
@@ -1,13 +1,16 @@
 drop table if exists users;
 create table users (
 	id serial primary key,
-	username text not null unique,
-	email text not null unique,
+	username text not null,
+	email text not null,
 	password text not null,
 	name text not null,
-	veil text not null unique,
+	veil text not null,
 	is_enabled boolean not null,
-	description text
+	description text,
+	constraint users_username_key unique (username),
+	constraint users_email_key unique (email),
+	constraint users_veil_key unique (veil)
 );
 INSERT INTO users (username, email, password, name, veil, is_enabled) VALUES
 	('demo', 'demo@example.com', '$2a$10$c4JbjbzXEIwC2fVqUIDzaen2qIUGuK7L8lq2LQpvnu2kNbrMwaa1G', 'Demo User', 'anonymous1', false)
