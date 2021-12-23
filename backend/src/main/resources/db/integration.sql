2f828233c9a843a9663f7247fdda1382dfc9c280
John Ballesteros <johnshift.dev@gmail.com>
2021-12-23 08:01:59 +0800

    add unique constrained on hibernate entity

--- backend/src/main/resources/db/integration.sql
+++ backend/src/main/resources/db/integration.sql
@@ -1,16 +1,13 @@
 drop table if exists users;
 create table users (
 	id serial primary key,
-	username text not null,
-	email text not null,
+	username text not null unique,
+	email text not null unique,
 	password text not null,
 	name text not null,
-	veil text not null,
+	veil text not null unique,
 	is_enabled boolean not null,
-	description text,
-	constraint users_username_key unique (username),
-	constraint users_email_key unique (email),
-	constraint users_veil_key unique (veil)
+	description text
 );
 INSERT INTO users (id, username, email, password, name, veil, is_enabled) VALUES
-	(99999, 'demo', 'demo@example.com', '$2a$10$c4JbjbzXEIwC2fVqUIDzaen2qIUGuK7L8lq2LQpvnu2kNbrMwaa1G', 'Demo User', 'anonymous1', false)
+	(99999,'demo', 'demo@example.com', '$2a$10$c4JbjbzXEIwC2fVqUIDzaen2qIUGuK7L8lq2LQpvnu2kNbrMwaa1G', 'Demo User', 'anonymous1', false)
