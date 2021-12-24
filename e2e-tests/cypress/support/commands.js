50c55b19d9c739f5a20522b80bbc07f9fbea17bb
John Ballesteros <johnshift.dev@gmail.com>
2021-12-24 09:20:36 +0800

    add e2e-test for login authorization check

--- e2e-tests/cypress/support/commands.js
+++ e2e-tests/cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("getAuthorization", () => {
+  return window.localStorage.getItem("authorization");
+});
