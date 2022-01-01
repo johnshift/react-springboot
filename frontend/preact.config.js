a10d5de2e07ff5b34c099bf3098737abc2681d8e
John Ballesteros <johnshift.dev@gmail.com>
2022-01-01 22:36:02 +0800

    setup preact + linaria

---
+++ frontend/preact.config.js
@@ -0,0 +1,28 @@
+export default (config, _env, helpers) => {
+  const { options, ...babelLoaderRule } = config.module.rules[0]; // Get the babel rule and options
+  options.presets.push(
+    "@babel/preset-typescript",
+    "@babel/preset-react",
+    "@linaria"
+  ); // Push the necessary presets
+  config.module.rules[0] = {
+    ...babelLoaderRule,
+    loader: undefined, // Disable the predefined babel-loader on the rule
+    use: [
+      {
+        loader: "babel-loader",
+        options,
+      },
+      {
+        loader: "@linaria/webpack-loader",
+        options: {
+          babelOptions: options, // Pass the current babel options to linaria's babel instance
+        },
+      },
+    ],
+  };
+
+  // disable css-modules for linaria
+  const css = helpers.getLoadersByName(config, "css-loader")[0];
+  css.loader.options.modules = false;
+};
