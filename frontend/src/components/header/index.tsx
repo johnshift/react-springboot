507dbe96228dcd8bdba044e8f46a9394fb7e287e
John Ballesteros <johnshift.dev@gmail.com>
2022-01-03 21:01:44 +0800

    setup vanilla-extract-css

--- frontend/src/components/header/index.tsx
+++ frontend/src/components/header/index.tsx
@@ -1,24 +1,19 @@
-import { FunctionalComponent, h } from 'preact';
-import { Link } from 'preact-router/match';
-import style from './style.css';
+import { FunctionalComponent, h } from "preact";
+import { Link } from "preact-router/match";
+
+import * as styles from "./header.css";
 
 const Header: FunctionalComponent = () => {
-    return (
-        <header class={style.header}>
-            <h1>Preact App</h1>
-            <nav>
-                <Link activeClassName={style.active} href="/">
-                    Home
-                </Link>
-                <Link activeClassName={style.active} href="/profile">
-                    Me
-                </Link>
-                <Link activeClassName={style.active} href="/profile/john">
-                    John
-                </Link>
-            </nav>
-        </header>
-    );
+  return (
+    <header>
+      <h1 class={styles.title}>Preact App</h1>
+      <nav>
+        <Link href="/">Home</Link>
+        <Link href="/profile">Me</Link>
+        <Link href="/profile/john">John</Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
