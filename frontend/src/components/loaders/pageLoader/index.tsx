ad8c5fd01cc786ce40a359bbd38cb1bdb56bff7c
John Ballesteros <johnshift.dev@gmail.com>
2022-01-09 12:17:15 +0800

    inline page loader css

--- frontend/src/components/loaders/pageLoader/index.tsx
+++ frontend/src/components/loaders/pageLoader/index.tsx
@@ -1,12 +1,10 @@
-import * as styles from './pageLoader.css';
-
 interface Props {
   text?: string;
 }
 
 const PageLoader = ({ text = 'loading ...' }: Props) => {
   return (
-    <div class={styles.wrapper}>
+    <div id="pgld">
       {/* replace this as logo / spinner */}
       <h1>{text}</h1>
     </div>
--- frontend/src/components/loaders/pageLoader/pageLoader.css.ts
+++
@@ -1,8 +0,0 @@
-import { style } from '@vanilla-extract/css';
-
-export const wrapper = style({
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  height: '100vh',
-});
