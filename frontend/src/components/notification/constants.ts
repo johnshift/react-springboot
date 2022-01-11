8d5a5cff56fced400eb85b8a7e6811155f762eb9
John Ballesteros <johnshift.dev@gmail.com>
2022-01-11 15:30:48 +0800

    working notification component

---
+++ frontend/src/components/notification/constants.ts
@@ -0,0 +1,10 @@
+export const NOTIF_SOMETHING_WENT_WRONG = 'Something went wrong :(';
+export const NOTIF_INCORRECT_LOGIN = 'Incorrect username/email or password';
+export const NOTIF_LOADING = 'Loading please wait';
+export const NOTIF_LOADING_LONGER = 'Loading longer than usual';
+
+export const NOTIF_TYPE_SUCCESS = 'success';
+export const NOTIF_TYPE_ERROR = 'error';
+export const NOTIF_TYPE_LOADING = 'loading';
+export const NOTIF_TYPE_INFO = 'info';
+export const NOTIF_TYPE_LONG = 'long';
