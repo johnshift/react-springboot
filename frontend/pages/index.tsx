94747c422ef7f99a9430aabd8b1e5fc7b8e6ca89
John Ballesteros <johnshift.dev@gmail.com>
2022-01-13 23:29:07 +0800

    move loginform dialog to new component

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,14 @@
+import { useState } from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 
+import Button from "@mui/material/Button";
+
+import LoginFormDialog from "../features/login/LoginFormDialog";
+
 const Home: NextPage = () => {
+  const [showLogin, setShowLogin] = useState(false);
+
   return (
     <>
       <Head>
@@ -13,6 +20,14 @@ const Home: NextPage = () => {
       </Head>
 
       <h1>Hello final-stack!</h1>
+
+      <div style={{ position: "fixed", top: 10, right: 10 }}>
+        <Button variant="contained" onClick={() => setShowLogin(true)}>
+          show login
+        </Button>
+      </div>
+
+      <LoginFormDialog open={showLogin} onClose={() => setShowLogin(false)} />
     </>
   );
 };
