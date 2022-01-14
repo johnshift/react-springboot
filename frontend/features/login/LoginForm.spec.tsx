91ac49133fe9349bc286aa8a6f2e48170d0d8cdc
John Ballesteros <johnshift.dev@gmail.com>
2022-01-14 08:24:03 +0800

    refactor login form skeleton to own component

---
+++ frontend/features/login/LoginForm.spec.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+
+import LoginForm from "./LoginForm";
+
+describe("LoginForm", () => {
+  test.todo("visibility");
+  test.todo("show password");
+  test.todo("escape/enter submits");
+  test.todo("empty principal");
+  test.todo("empty password");
+  test.todo("short principal");
+  test.todo("short password");
+  test.todo("long principal");
+  test.todo("long password");
+  test.todo("principal neat-uri");
+  test.todo("principal invalid email");
+  test.todo("backend network error");
+  test.todo("show loading when valid submit");
+  test.todo("show loading longer than usual");
+  test.todo("incorrect login");
+  test.todo("successful login + token persist");
+});
