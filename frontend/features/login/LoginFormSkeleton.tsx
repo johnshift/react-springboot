a8f6527d657dfbe4e1554b8782da66b0633ac4b4
John Ballesteros <johnshift.dev@gmail.com>
2022-01-14 08:36:48 +0800

    extract skeleton component as reusable

--- frontend/features/login/LoginFormSkeleton.tsx
+++ frontend/features/login/LoginFormSkeleton.tsx
@@ -1,43 +1,15 @@
-import { Skeleton, Box, Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
+import Skeleton from "../../components/Skeleton";
 
 const LoginFormSkeleton = () => {
   return (
     <Stack spacing={3} data-testid="loginForm-skl">
-      <Skeleton
-        variant="rectangular"
-        sx={{
-          height: 56,
-          width: 265,
-          borderRadius: "10px",
-        }}
-        animation="wave"
-      />
-      <Skeleton
-        variant="rectangular"
-        sx={{
-          height: 56,
-          width: 265,
-          borderRadius: "10px",
-        }}
-        animation="wave"
-      />
+      <Skeleton height={56} width={265} />
+      <Skeleton height={56} width={265} />
 
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Skeleton
-          variant="rectangular"
-          sx={{ width: 140, height: 25, borderRadius: "5px" }}
-          animation="wave"
-        />
-
-        <Skeleton
-          variant="rectangular"
-          sx={{
-            height: 36.5,
-            width: 76,
-            borderRadius: "10px",
-          }}
-          animation="wave"
-        />
+        <Skeleton height={25} width={140} borderRadius="5px" />
+        <Skeleton height={36.5} width={76} />
       </Box>
     </Stack>
   );
