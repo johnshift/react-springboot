21a3845042983ec52379a2963400b82a8774242a
John Ballesteros <johnshift.dev@gmail.com>
2022-01-19 20:58:50 +0800

    cleanup 100 coverage all

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,33 +1,7 @@
-import Toast from "../components/toast";
-import useToast from "../components/toast/useToast";
-
 const Home = () => {
-  const { msg, show, severity, toastClose, toastLoading } = useToast();
-
   return (
     <div>
       <h1>Home page</h1>
-      <button
-        onClick={() => {
-          console.log("button click");
-          toastLoading();
-        }}
-      >
-        show toast
-      </button>
-
-      <p>msg = {msg}</p>
-      <p>show = {show.toString()}</p>
-
-      <br />
-      <br />
-      <Toast
-        show={show}
-        msg={msg}
-        severity={severity}
-        onExited={toastClose}
-        ignoreClickAway={false}
-      />
     </div>
   );
 };
