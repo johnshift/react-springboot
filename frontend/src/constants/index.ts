bccf1389d479ea2dc46aa984c080c7582282aebf
John Ballesteros <johnshift.dev@gmail.com>
2022-01-19 20:58:50 +0800

    cleanup 100 coverage all

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,8 +1,1 @@
-import { ToastPosition } from "react-hot-toast";
-
-export const TOAST_OPTIONS = {
-  id: "app-toast",
-  position: "bottom-center" as ToastPosition,
-};
-
 export const MSG_SOMETHING_WENT_WRONG = "Something went wrong :(";
