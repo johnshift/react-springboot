35eccafe1eb4feb34c37a4072c6dede465bd4fa9
John Ballesteros <johnshift.dev@gmail.com>
2022-02-01 16:35:58 +0800

    add simple pretty-route implementation frontend

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,4 +1,5 @@
-import { Button } from "@mui/material";
+import { Button, Link as MuiLink } from "@mui/material";
+import Link from "next/link";
 import useDisclosure from "../common/hooks/useDisclosure";
 import LoginFormDialog from "../features/login/LoginFormDialog";
 import useToast from "../features/toast/useToast";
@@ -15,6 +16,30 @@ const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
+      <Link href="/demo" passHref>
+        <MuiLink underline="hover" color="inherit">
+          /demo
+        </MuiLink>
+      </Link>
+      <br />
+      <br />
+      <br />
+      <Link href="/anonymous1" passHref>
+        <MuiLink underline="hover" color="inherit">
+          /anonymous1
+        </MuiLink>
+      </Link>
+      <br />
+      <br />
+      <br />
+      <Link href="/asdf" passHref>
+        <MuiLink underline="hover" color="inherit">
+          /asdf
+        </MuiLink>
+      </Link>
+      <br />
+      <br />
+      <br />
       <button onClick={toastLoading}>toast loading home-page</button>
 
       <div style={{ position: "fixed", top: 10, right: 10 }}>
