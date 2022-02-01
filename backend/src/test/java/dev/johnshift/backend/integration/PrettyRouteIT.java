5a991ac3a7df7607b77f043ef09f395fd688ee1b
John Ballesteros <johnshift.dev@gmail.com>
2022-02-01 15:12:46 +0800

    add pretty-route backend integration tests

---
+++ backend/src/test/java/dev/johnshift/backend/integration/PrettyRouteIT.java
@@ -0,0 +1,87 @@
+package dev.johnshift.backend.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.cors.CorsConfigurationSource;
+import dev.johnshift.backend.repositories.UserRepository;
+import dev.johnshift.backend.services.UserService;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class PrettyRouteIT {
+
+	@Autowired
+	PasswordEncoder passwordEncoder;
+
+	@Autowired
+	CorsConfigurationSource corsConfigurationSource;
+
+	@Autowired
+	UserDetailsService userDetailsService;
+
+	@Autowired
+	DaoAuthenticationProvider daoAuthenticationProvider;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	UserService userService;
+
+	@Autowired
+	WebTestClient webTestClient;
+
+	private final String API_URI = "/api/v1/pretty-route";
+
+	@Test
+	void invalidType_OK() {
+
+		String name = "non-existing";
+
+		webTestClient.get()
+			.uri(API_URI + "/" + name)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+			.jsonPath("$.name").isEqualTo(name)
+			.jsonPath("$.type").isEqualTo("NOT_FOUND");
+
+	}
+
+	@Test
+	void profileType_OK() {
+
+		String name = "demo";
+
+		webTestClient.get()
+			.uri(API_URI + "/" + name)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+			.jsonPath("$.name").isEqualTo(name)
+			.jsonPath("$.type").isEqualTo("PROFILE");
+
+	}
+
+	@Test
+	void veilType_OK() {
+
+		String name = "anonymous1";
+
+		webTestClient.get()
+			.uri(API_URI + "/" + name)
+			.exchange()
+			.expectStatus().isOk()
+			.expectBody()
+			.jsonPath("$.name").isEqualTo(name)
+			.jsonPath("$.type").isEqualTo("VEIL");
+
+	}
+
+
+}
