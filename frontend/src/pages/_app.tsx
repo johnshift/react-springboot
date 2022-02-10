f67c3164129a4fea88f7b08b09d22754010b51da
John Ballesteros <johnshift.dev@gmail.com>
2022-02-10 21:36:23 +0800

    working mention using @ inside textarea

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import * as React from "react";
 import type { AppProps } from "next/app";
 
+import "../styles/global.css";
+
 import { CacheProvider, EmotionCache } from "@emotion/react";
 import {
   ThemeProvider,
