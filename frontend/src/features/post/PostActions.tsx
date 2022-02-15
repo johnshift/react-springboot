a2dcf745e54310e902cdb53d23cf235b32bb74e4
John Ballesteros <johnshift.dev@gmail.com>
2022-02-15 11:24:42 +0800

    minor fixes ui improvement

--- frontend/src/features/post/PostActions.tsx
+++ frontend/src/features/post/PostActions.tsx
@@ -49,7 +49,7 @@ const PostActions = () => {
                 <path d="M13 14h-2a8.999 8.999 0 00-7.968 4.81A10.136 10.136 0 013 18C3 12.477 7.477 8 13 8V3l10 8-10 8v-5z" />
               </SvgIcon>
             </IconButton>
-            <IconButton>
+            <IconButton onClick={clickReact}>
               <TagFacesIcon />
             </IconButton>
           </>
